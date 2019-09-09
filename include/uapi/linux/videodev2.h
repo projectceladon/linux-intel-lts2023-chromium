0515dc9d2fe28a3991e1f425fe2f48cb657841ee
Jerry-ch Chen <jerry-ch.chen@mediatek.com>
2019-09-09 16:12:25 +0800

    BACKPORT: FROMLIST: media: platform: mediatek: isp: Add Mediatek FD driver
    This patch adds the driver of Face Detection (FD) unit in
    Mediatek camera system, providing face detection function.
    
    The mtk-isp directory will contain drivers for multiple IP
    blocks found in Mediatek ISP system. It will include ISP Pass 1
    driver (CAM), sensor interface driver, DIP driver and face
    detection driver.
    
    (am from https://patchwork.kernel.org/patch/11134913/)
    
    Set offset of MTK FD control ID base to 0x10c0 in
    include/uapi/linux/v4l2-controls.h
    
    Sovle conflicts in Kconfig and Makefile
    
    Signed-off-by: Jerry-ch Chen <jerry-ch.chen@mediatek.com>
    
    cherry-pick from v4.19 and squash all the FD relatived patches as below:
    2825538: BACKPORT: FROMLIST: platform: mtk-isp: Add Mediatek FD driver
    2825539: FIXUP: BACKPORT: FROMLIST: platform: mtk-isp: Add Mediatek FD driver
    2825540: FIXUP: BACKPORT: FROMLIST: platform: mtk-isp: Add Mediatek FD driver
    2825541: FIXUP: BACKPORT: FROMLIST: platform: mtk-isp: Add Mediatek FD driver (take 2)
    2825542: FIXUP: BACKPORT: FROMLIST: platform: mtk-isp: Add Mediatek FD driver (take 3)
    2828015: FIXUP: BACKPORT: FROMLIST: platform: mtk-isp: Add Mediatek FD driver (take 4)
    2828018: CHROMIUM: FIXUP: remoteproc/mediatek: add SCP support for mt8183
    2828020: CHROMIUM: platform: mtk-isp: set plane address with data_offset
    2828021: FIXUP: BACKPORT: FROMLIST: platform: mtk-isp: Add Mediatek FD driver
    2828023: CHROMIUM: media: mtk-isp: Avoid infinite recursion
    
    Signed-off-by: Bibby Hsieh <bibby.hsieh@mediatek.com>
    Signed-off-by: Hsin-Yi Wang <hsinyi@chromium.org>
    
    Cherry-pick from v5.10 CL:2825538, fix conflicts and move the isp folder into the mediaTek folder.
    
    BUG=b:341019453
    TEST=build pass
    UPSTREAM-TASK=b:368493852
    
    Change-Id: I1d1ba58cbdcdcc161b140398fc26b24ec2134cdb
    Signed-off-by: Olivia Wen <olivia.wen@mediatek.corp-partner.google.com>
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/third_party/kernel/+/5733717
    Tested-by: Hsin-Te Yuan <yuanhsinte@chromium.org>
    Reviewed-by: Hsin-Te Yuan <yuanhsinte@chromium.org>
    Commit-Queue: Hsin-Te Yuan <yuanhsinte@chromium.org>

--- include/uapi/linux/videodev2.h
+++ include/uapi/linux/videodev2.h
@@ -877,6 +877,9 @@ struct v4l2_pix_format {
 #define V4L2_META_FMT_RK_ISP1_PARAMS	v4l2_fourcc('R', 'K', '1', 'P') /* Rockchip ISP1 3A Parameters */
 #define V4L2_META_FMT_RK_ISP1_STAT_3A	v4l2_fourcc('R', 'K', '1', 'S') /* Rockchip ISP1 3A Statistics */
 
+/* Vendor specific - Mediatek Face Detection meta buffer format for firmware */
+#define V4L2_META_FMT_MTFD_RESULT  v4l2_fourcc('M', 'T', 'f', 'd') /* FD meta capture buffer */
+
 /* priv field value to indicates that subsequent fields are valid. */
 #define V4L2_PIX_FMT_PRIV_MAGIC		0xfeedcafe
 
