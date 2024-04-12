aa7c4fec2ffa3bdf21df0211b8f49f488de86dd0
Stephen Boyd <swboyd@chromium.org>
2024-04-12 00:46:07 -0700

    Revert "FROMLIST: clk: Don't hold prepare_lock when calling kref_put()"
    This reverts commit fbb41f438e5d4458e7fc838abf6b69adbfe93fbf. We're
    going to pick FROMGIT.
    
    BUG=b:331232181
    TEST=git diff
    
    Change-Id: Iae24fd5bdf906c2117ee0573a954488a1898de70
    Signed-off-by: Stephen Boyd <swboyd@chromium.org>
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/third_party/kernel/+/5449574
    Reviewed-by: Douglas Anderson <dianders@chromium.org>

--- drivers/clk/clk.c
+++ drivers/clk/clk.c
@@ -4420,8 +4420,7 @@ void clk_unregister(struct clk *clk)
 	if (ops == &clk_nodrv_ops) {
 		pr_err("%s: unregistered clock: %s\n", __func__,
 		       clk->core->name);
-		clk_prepare_unlock();
-		return;
+		goto unlock;
 	}
 	/*
 	 * Assign empty clock ops for consumers that might still hold
@@ -4455,10 +4454,11 @@ void clk_unregister(struct clk *clk)
 	if (clk->core->protect_count)
 		pr_warn("%s: unregistering protected clock: %s\n",
 					__func__, clk->core->name);
-	clk_prepare_unlock();
 
 	kref_put(&clk->core->ref, __clk_release);
 	free_clk(clk);
+unlock:
+	clk_prepare_unlock();
 }
 EXPORT_SYMBOL_GPL(clk_unregister);
 
@@ -4617,11 +4617,13 @@ void __clk_put(struct clk *clk)
 	if (clk->min_rate > 0 || clk->max_rate < ULONG_MAX)
 		clk_set_rate_range_nolock(clk, 0, ULONG_MAX);
 
-	clk_prepare_unlock();
-
 	owner = clk->core->owner;
 	kref_put(&clk->core->ref, __clk_release);
+
+	clk_prepare_unlock();
+
 	module_put(owner);
+
 	free_clk(clk);
 }
 
