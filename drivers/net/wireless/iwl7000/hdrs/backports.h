128d2f44204b39ce1d816b7b256352550a3014fd
Johannes Berg <johannes.berg@intel.com>
2024-04-26 11:52:38 +0200

    CHROMIUM: iwl7000: clean up ieee80211_tx_control_port()
    Clean up ieee80211_tx_control_port() to only have the version as
    needed, without all the indirection (which even uses a function
    pointer to an inline.)
    
    Change-Id: I3c01fdda7623ce3d4f8dbc8f37d5f69f26b1533e
    Signed-off-by: Johannes Berg <johannes.berg@intel.com>
    tested: iil_jenkins iil_jenkins <EC.GER.UNIX.IIL.JENKINS@INTEL.COM>
    iwl7000-tree: a76aec20b4507d690f92b856f693d0387f0e53e4

--- drivers/net/wireless/iwl7000/hdrs/backports.h
+++ drivers/net/wireless/iwl7000/hdrs/backports.h
@@ -27,11 +27,6 @@ static inline bool iwl7000_wiphy_ext_feature_isset(struct wiphy *wiphy,
 #define wiphy_ext_feature_set iwl7000_wiphy_ext_feature_set
 #define wiphy_ext_feature_isset iwl7000_wiphy_ext_feature_isset
 
-int ieee80211_tx_control_port(struct wiphy *wiphy, struct net_device *dev,
-			      const u8 *buf, size_t len,
-			      const u8 *dest, __be16 proto, bool unencrypted,
-			      int link_id, u64 *cookie);
-
 void ieee80211_fragment_element(struct sk_buff *skb, u8 *len_pos, u8 frag_id);
 
 static inline void
