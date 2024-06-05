5edbd180cbd01e05ac2da4d815552cc325a5e40b
Emmanuel Grumbach <emmanuel.grumbach@intel.com>
2024-06-05 22:03:04 +0300

    CHROMIUM: iwl7000: mld: add a kunit test to check hcmd_names are sorted
    Change-Id: I81363e2812da27ca778b67878f7d5fd81d980341
    Signed-off-by: Emmanuel Grumbach <emmanuel.grumbach@intel.com>
    tested: iil_jenkins iil_jenkins <EC.GER.UNIX.IIL.JENKINS@INTEL.COM>
    iwl7000-tree: 3f15c275ca3504209be8ba625898ce4ef4aa50a6

--- drivers/net/wireless/iwl7000/iwlwifi/mld/mld.c
+++ drivers/net/wireless/iwl7000/iwlwifi/mld/mld.c
@@ -106,12 +106,14 @@ static const struct iwl_hcmd_names iwl_mld_reg_and_nvm_names[] = {
 	HCMD_NAME(NVM_GET_INFO),
 };
 
-static const struct iwl_hcmd_arr iwl_mld_groups[] = {
+VISIBLE_IF_IWLWIFI_KUNIT
+const struct iwl_hcmd_arr iwl_mld_groups[] = {
 	[LEGACY_GROUP] = HCMD_ARR(iwl_mld_legacy_names),
 	[LONG_GROUP] = HCMD_ARR(iwl_mld_long_names),
 	[SYSTEM_GROUP] = HCMD_ARR(iwl_mld_system_names),
 	[REGULATORY_AND_NVM_GROUP] = HCMD_ARR(iwl_mld_reg_and_nvm_names),
 };
+EXPORT_SYMBOL_IF_IWLWIFI_KUNIT(iwl_mld_groups);
 
 static void
 iwl_mld_configure_trans(struct iwl_op_mode *op_mode)
