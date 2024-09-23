642da25adb85234dfa6fb07b5ee85c4753a822ec
yerriswamy <yerriswamy.kt@intel.com>
2024-09-23 15:09:27 +0530

    Revert "eventfs: Use SRCU for freeing eventfs_inodes"
    This commit is reverted because all the base code related to
    this commit has been removed
    
    This reverts commit 2ef3d120d19faba3dec35b10a025b00db0284373.
    
    Signed-off-by: yerriswamy <yerriswamy.kt@intel.com>

--- fs/tracefs/event_inode.c
+++ fs/tracefs/event_inode.c
@@ -84,7 +84,7 @@ static void release_ei(struct kref *ref)
 	kfree_const(ei->name);
 	kfree_rcu(ei, rcu);
 
-	call_srcu(&eventfs_srcu, &ei->rcu, free_ei_rcu);
+	call_rcu(&ei->rcu, free_ei_rcu);
 }
 
 static inline void put_ei(struct eventfs_inode *ei)
